ed012acc4b78cc75cf4103779c36cb6aa57d763e
aurora-bee <a.bubnyak@gmail.com>
2025-12-28 22:26:11 +0100

    basic gameplay working yay :3

---
+++ levels/mini-snake/mini_snake.gd
@@ -0,0 +1,100 @@
+extends Level
+
+const WHITE := Vector2i(0,0)
+const BLACK := Vector2i(1,0)
+const BORDER_RIGHT := 19
+const BORDER_BOTTOM := 10
+
+var snake_head := Vector2i(5,5)
+var snake_body: Array[Vector2i] = [Vector2i(4,5),Vector2i(3,5),Vector2i(2,5)]
+var snake_dir := Vector2i.RIGHT
+var has_eaten := false
+
+var apples: Array[Vector2i] = []
+
+@onready var tilemap := $TileMapLayer
+
+var tick := 0
+var every_th_tick := 8
+
+func _ready() -> void:
+	for i in 4:
+		var apple_position := get_random_position()
+		while !is_valid_apple_position(apple_position):
+			apple_position = get_random_position()
+		apples.append(apple_position)
+
+# more explicit with ifs to make conditions more readable
+func is_valid_apple_position(apple_position: Vector2i) -> bool:
+	# Let's block off the area inside and immediately in front of the snake
+	if apple_position.y == snake_head.y:
+		return false
+	# We also don't want overlapping apples
+	if apple_position in apples:
+		return false
+	return true
+
+func get_random_position() -> Vector2i:
+	var x := randi() % (BORDER_RIGHT - 1) + 1 # 1-18
+	var y := randi() % (BORDER_BOTTOM - 1) + 1 # 1-9
+	return Vector2i(x, y)
+
+func _physics_process(_delta: float) -> void:
+	tick = tick + 1
+	if tick % every_th_tick == 0:
+		process_snake_move()
+		check_collision()
+		check_win()
+
+func process_snake_move() -> void:
+	if not has_eaten:
+		snake_body.resize(snake_body.size() - 1)
+	snake_body.push_front(snake_head)
+	snake_head = snake_head + snake_dir
+	has_eaten = false
+
+func check_collision() -> void:
+	if snake_head.x == BORDER_RIGHT or snake_head.x == 0 or snake_head.y == BORDER_BOTTOM or snake_head.y == 0:
+		lose.emit()
+	elif snake_head in snake_body:
+		lose.emit()
+	elif snake_head in apples:
+		apples.erase(snake_head)
+		has_eaten = true
+		
+func check_win() -> void:
+	if apples.is_empty():
+		win.emit()
+
+func _process(_delta: float) -> void:
+	clear_board()
+	draw_snake()
+	draw_apples()
+
+func clear_board() -> void:
+	for x in range(1,19):
+		for y in range(1,9):
+			tilemap.set_cell(Vector2i(x,y), 0, BLACK)
+
+func draw_snake() -> void:
+	tilemap.set_cell(snake_head, 0, WHITE)
+	for cell in snake_body:
+		tilemap.set_cell(cell, 0, WHITE)
+
+func draw_apples() -> void:
+	for cell in apples:
+		tilemap.set_cell(cell, 0, WHITE)
+		
+func _input(event: InputEvent) -> void:
+	var new_dir := Vector2i.ZERO
+	if event.is_action_pressed("move_up"):
+		new_dir = Vector2i.UP
+	elif event.is_action_pressed("move_down"):
+		new_dir = Vector2i.DOWN
+	elif event.is_action_pressed("move_left"):
+		new_dir = Vector2i.LEFT
+	elif event.is_action_pressed("move_right"):
+		new_dir = Vector2i.RIGHT
+	# avoid from turning snake inward, and don't change dir if keypress wasn't WASD
+	if snake_dir + new_dir != Vector2i.ZERO and new_dir != Vector2i.ZERO:
+		snake_dir = new_dir
